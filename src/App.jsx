4b0117889e131d4a97c122048ee070bdf1ffd978
Danil <stepanova.nata.r@gmail.com>
2022-10-02 02:34:11 +0300

    Нужно допилить форму добавления новых записей

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,48 @@
-import React, {useState} from 'react';
-import Counter from "./components/Counter";
+import React, { useState} from 'react';
 import './css/App.css';
-import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyBtn from "./components/UI/Button/MyBtn";
+import MyInput from "./components/UI/Input/MyInput";
 
 
 function App() {
-	const [posts, setPost] = useState([
+	const [posts, setPosts] = useState([
 		{id:1, title: 'Javascript', body: 'Description'},
 		{id:2, title: 'Javascript 2', body: 'Description'},
 		{id:3, title: 'Javascript 3', body: 'Description'}
 	])
 	
+	// const bodyInputRef = useRef();
+	const [post, setPost] = useState({title: '', body: ''})
+	const addNewPost = (e) => {
+		e.preventDefault();
+		setPosts([...posts, {...post, id: Date.now()}]);
+		setPost({title: '', body: ''})
+	}
+	
 	
 	return (
 		<div className='App'>
+			<form action="#">
+				{/*Управляемый компонент*/}
+				<MyInput
+					value = {post.title}
+					onChange = {e => setPost({...post, title: e.target.value})}
+					type="text"
+					placeholder='Название поста'/>
+				<MyInput
+					value = {post.body}
+					onChange = {e => setPost({...post, body: e.target.value})}
+					type="text"
+					placeholder='Описание поста'/>
+				
+				{/*Неуправляемый или неконтролируемый компонент*/}
+				{/*<MyInput*/}
+				{/*	ref={bodyInputRef}*/}
+				{/*	type="text"*/}
+				{/*	placeholder='Описание поста'/>*/}
+				<MyBtn onClick={addNewPost}>Создать пост</MyBtn>
+			</form>
 			<PostList posts={posts} title={'Список постов JS'}/>
 
 		</div>
